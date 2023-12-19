f63684331515561d0492c97bc7592f6b73b928f7
sufiyan <sp3596894@gmail.com>
2023-12-19 21:26:37 +0530

    attached routes with controllers

--- backend/controllers/controllerController.js
+++ backend/controllers/controllerController.js
@@ -18,16 +18,16 @@ const createRouter = (body) => {
     
     for (let i = 0; i <= methods.length - 1; i++) {
       if (methods[i] == "get") {
-        const content2 = "const getUsers = async () => { \ntry { \nconst users = await User.find(); \nreturn users; \n} catch (error) { \nthrow error; \n} \n}; \n\n"
+        const content2 = "const getUsers = async () => { \ntry { \nconst users = await "+ body.models[i] + ".find(); \nreturn users; \n} catch (error) { \nthrow error; \n} \n}; \n\n"
         combinedContent += content2;
       } else if (methods[i] == "post") {
-        const content2 = "const createUser = async (body) => { \ntry { \nconst newUser = new User(body); \nawait newUser.save(); \nreturn newUser; \n} catch (error) { \nthrow error; \n} \n}; \n\n"
+        const content2 = "const createUser = async (body) => { \ntry { \nconst newUser = new "+ body.models[i] + "(body); \nawait newUser.save(); \nreturn newUser; \n} catch (error) { \nthrow error; \n} \n}; \n\n"
         combinedContent += content2;
       } else if (methods[i] == "put") {
-        const content2 = "const updateUser = async (id, body) => { \ntry { \nconst user = await User.findByIdAndUpdate(id, body, { new: true }); \nreturn user; \n} catch (error) { \nthrow error; \n} \n}; \n\n"
+        const content2 = "const updateUser = async (id, body) => { \ntry { \nconst user = await "+ body.models[i] + ".findByIdAndUpdate(id, body, { new: true }); \nreturn user; \n} catch (error) { \nthrow error; \n} \n}; \n\n"
         combinedContent += content2;
       } else if (methods[i] == "delete") {
-        const content2 = "const deleteUser = async (id) => { \ntry { \nconst result = await User.findByIdAndDelete(id); \nreturn result !== null; \n} catch (error) { \nthrow error; \n} \n}; \n\n"
+        const content2 = "const deleteUser = async (id) => { \ntry { \nconst result = await "+ body.models[i] + ".findByIdAndDelete(id); \nreturn result !== null; \n} catch (error) { \nthrow error; \n} \n}; \n\n"
         combinedContent += content2;
       }
     }
@@ -53,7 +53,7 @@ const createRouter = (body) => {
     const content5 = "};"
     combinedContent += content5
     
-    const txtFilePath = path.join("./Code/controllers", body.route + '.js');
+    const txtFilePath = path.join("./Code/controllers", body.route + 'Controller.js');
 
     fs.writeFileSync(txtFilePath, combinedContent, 'utf-8');
 
--- backend/controllers/routerController.js
+++ backend/controllers/routerController.js
@@ -7,38 +7,44 @@ const doubleQuoteRemover = require('./doubleQuoteRemover')
 const controllerController = require('./controllerController')
 
 // code for new Schema creation
-const createRouter = async(body) => {
-  const data = await controllerController.createRouter(body)
-  /*
-  methods = body.methods;
+const createRouter = async (body) => {
+
+  await controllerController.createRouter(body)
+
   let combinedContent = ""
-  const content1 = "const express = require('express'); \nconst bodyParser = require('body-parser'); \nconst userController = require('../controllers/userController'); \n"
-  const content1_2 = "const router = express.Router(); \nrouter.use(bodyParser.json());"
+  const content1 = "const express = require('express'); \nconst bodyParser = require('body-parser'); \n"
   combinedContent += content1;
+
+  controllers = body.models;
+  
+  const content2 = "const " + body.route.split('/')[1] + "Controller = require('../controllers/" + body.route.split('/')[1] + "Controller'); \n\n"
+  combinedContent += content2
+  const content1_2 = "const router = express.Router(); \nrouter.use(bodyParser.json());\n\n"
   combinedContent += content1_2
+
+  methods = body.methods;
   for (let i = 0; i <= methods.length - 1; i++) {
     if (methods[i] == "get") {
-      const content2 = "router." + methods[i] + "(" + "'" + body.route + "'" + ", async (req, res) => { \ntry { \nconst allUsers = await userController.getUsers(); \nres.json(allUsers); \n} catch (error) { \nres.status(500).json({ error: 'Internal Server Error' }); \n}}); \n"
+      const content2 = "router." + methods[i] + "(" + "'" + body.route + "'" + ", async (req, res) => { \ntry { \nconst allUsers = await " + body.route.split('/')[1] +"Controller.getUsers(); \nres.json(allUsers); \n} catch (error) { \nres.status(500).json({ error: 'Internal Server Error' }); \n}}); \n\n"
       combinedContent += content2;
     } else if (methods[i] == "post") {
-      const content2 = "router." + methods[i] + "(" + "'" + body.route + "'" + ", async (req, res) => { \nconst newData = req.body; \ntry { \nconst newUser = await userController.createUser(newData); \nres.status(201).json(newUser); \n} catch (error) { \nres.status(500).json({ error: 'Internal Server Error' }); \n}}); \n"
+      const content2 = "router." + methods[i] + "(" + "'" + body.route + "'" + ", async (req, res) => { \nconst newData = req.body; \ntry { \nconst newUser = await " + body.route.split('/')[1] +"Controller.createUser(newData); \nres.status(201).json(newUser); \n} catch (error) { \nres.status(500).json({ error: 'Internal Server Error' }); \n}}); \n\n"
       combinedContent += content2;
     } else if (methods[i] == "put") {
-      const content2 = "router." + methods[i] + "(" + "'" + body.route + "'" + ", async (req, res) => { \nconst newData = req.body; \ntry { \nconst newUser = await userController.updateUser(req.params.id, newData); \nres.status(201).json(newUser); \n} catch (error) { \nres.status(500).json({ error: 'Internal Server Error' }); \n}}); \n"
+      const content2 = "router." + methods[i] + "(" + "'" + body.route + "/:id'" + ", async (req, res) => { \nconst newData = req.body; \ntry { \nconst newUser = await " + body.route.split('/')[1] +"Controller.updateUser(req.params.id, newData); \nres.status(201).json(newUser); \n} catch (error) { \nres.status(500).json({ error: 'Internal Server Error' }); \n}}); \n\n"
       combinedContent += content2;
     } else if (methods[i] == "delete") {
-      const content2 = "router." + methods[i] + "(" + "'" + body.route + "'" + ", async (req, res) => { \nconst newData = req.body; \ntry { \nconst newUser = await userController.deleteUser(req.params.id, newData); \nres.status(201).json(newUser); \n} catch (error) { \nres.status(500).json({ error: 'Internal Server Error' }); \n}}); \n"
+      const content2 = "router." + methods[i] + "(" + "'" + body.route + "/:id'" + ", async (req, res) => { \nconst newData = req.body; \ntry { \nconst newUser = await " + body.route.split('/')[1] +"Controller.deleteUser(req.params.id, newData); \nres.status(201).json(newUser); \n} catch (error) { \nres.status(500).json({ error: 'Internal Server Error' }); \n}}); \n\n"
       combinedContent += content2;
     }
   }
   const content4 = "module.exports = router;"
   combinedContent += content4
-  
-  const txtFilePath = path.join("./Code/routes", body.route + '.js');
+
+  const txtFilePath = path.join("./Code/routes", body.route + 'Router.js');
 
   fs.writeFileSync(txtFilePath, combinedContent, 'utf-8');
 
-  */
   return body;
 }
 
