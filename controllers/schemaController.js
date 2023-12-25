89cce19a281540371101e0d99a3f2b30c889db88
sufiyan <sp3596894@gmail.com>
2023-12-25 20:50:00 +0530

    doublequote remover discarded

--- controllers/schemaController.js
+++ controllers/schemaController.js
@@ -2,13 +2,12 @@
 const fs = require('fs');
 const path = require('path');
 const util = require('util');
-const doubleQuoteRemover = require('../scripts/doubleQuoteRemover')
 
 // code for new Schema creation
 const createSchema = (body) => {
   const content1 = "const mongoose = require('mongoose');\n\n";
   const content2 = "const " + body.schemaName + "Schema = new mongoose.Schema(";
-  const content3 = doubleQuoteRemover(body.schema);
+  const content3 = body.schema;
   const content4 = ");\n\n";
   const content5 = "const " + body.schemaName + " = mongoose.model(" + '"' + body.schemaName + '"' + ", " + body.schemaName + "Schema" + ");\n\n"
   const content6 = "module.exports = " + body.schemaName + ";";
