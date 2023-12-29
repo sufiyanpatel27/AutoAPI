ded23de445ad24a9df7190922f81c1deeb68dde6
sufiyan <sp3596894@gmail.com>
2023-12-29 12:52:00 +0530

    added zip file creator

--- controllers/submitController.js
+++ controllers/submitController.js
@@ -2,6 +2,8 @@
 const fs = require('fs');
 const path = require('path');
 const util = require('util');
+const archiver = require('archiver');
+
 
 // code for Server creation
 const createCode = () => {
@@ -11,8 +13,8 @@ const createCode = () => {
         const content1 = "const express = require('express');\n";
         const content3 = "const mongoose = require('mongoose');\n";
         combinedContent += content1 + content3;
-        for (let i = 0; i <= files.length-1; i++) {
-            const content2 = "const "+ files[i].split('.')[0] +" = require('./routes/"+ files[i].split('.')[0] +"');\n";
+        for (let i = 0; i <= files.length - 1; i++) {
+            const content2 = "const " + files[i].split('.')[0] + " = require('./routes/" + files[i].split('.')[0] + "');\n";
             combinedContent += content2
         }
         const content5 = "\nconst PORT = process.env.PORT || 5000;\n"
@@ -22,8 +24,8 @@ const createCode = () => {
         const content9 = "db.once('open', () => { \nconsole.log('Connected to MongoDB'); \n});\n\n"
         const content10 = "const app = express();\n\n"
         combinedContent += content5 + content6 + content7 + content8 + content9 + content10
-        for (let i = 0; i <= files.length-1; i++) {
-            const content2 = "app.use('/', "+ files[i].split('.')[0] +");\n";
+        for (let i = 0; i <= files.length - 1; i++) {
+            const content2 = "app.use('/', " + files[i].split('.')[0] + ");\n";
             combinedContent += content2
         }
         const content11 = "\n"
@@ -37,6 +39,23 @@ const createCode = () => {
 
         // writing into the js file
         fs.writeFileSync(txtFilePath, combinedContent, 'utf-8');
+
+
+        //
+        const outputZip = fs.createWriteStream('Code.zip');
+
+        // Create a zip archive
+        const archive = archiver('zip', { zlib: { level: 9 } });
+
+        // Pipe the archive to the output stream
+        archive.pipe(outputZip);
+
+        // Add all files in the folder to the archive
+        archive.directory('Code', false);
+
+        // Finalize the archive
+        archive.finalize();
+
     });
 }
 
